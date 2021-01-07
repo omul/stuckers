f20274fc9a12323daa619cd01c37c0702d9d3548
Овчинников Михаил Юрьевич <ovchinnikovmy@rushydro.ru>
2021-01-07 17:32:31 +0900

    Start using bitboards

--- stuckers.py
+++ stuckers.py
@@ -1,45 +1,25 @@
 #checkers-stuckers
 DIM=8
 
-_table=[[0 for i in range(DIM)] for j in range(DIM)]
-_our=[(0, x,) for x in range(DIM) if x%2==0]
-_their=[(DIM-1, x,) for x in range(DIM) if (DIM+x)%2!=0]
+desc=0xaa55aa55aa55aa55
+our=[1,4,16,64]
+their=[0x8000000000000000, 0x2000000000000000,
+       0x0800000000000000, 0x0200000000000000]
 
-def print_table():
-    for i in reversed(_table):
-        print (i)
+def display(val):
+    s=format(val, 'b').zfill(64)[::-1]
+    out = ''
+    for i in range(64,0,-8):
+        out=out + s[i-8:i] + '\n'
+    print(out)
 
-def is_free_field(x, y, table=_table):
-    if x<0 or y<0 or x>DIM-1 or y>DIM-1:
-        return False
-    if (x+y)%2==0 and table[x][y]==0:
-        return True
-    else:
-        return False
-
-def count_movies(table=_table):
-    if 
+#def is_free_field(x, y, table=_table):
+#def count_movies(table=_table):
     
 def our_step():
-    counter=0
-    for x,y in _our:
-        if is_free_steps(x-1, y+1):
-            count_movies()
-        if is_free_steps(x+1, y+1):
-            count_movies()
-        counter+=1
-        
-      
+    none              
 
 def their_step():
     none
 
-
-for i,j in _our:
-    _table[i][j]=1
-
-for i,j in _their:
-    _table[i][j]=2
-
-
-print_table()
+display(desc)
