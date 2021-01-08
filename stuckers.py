f9ec9f9ae3e7a2a52757211f83c92b2f572e79a1
Овчинников Михаил Юрьевич <ovchinnikovmy@rushydro.ru>
2021-01-08 23:26:34 +0900

    Add russian desctiption

--- stuckers.py
+++ stuckers.py
@@ -1,11 +1,25 @@
 # checkers-stuckers
 DIM = 8
 
+class Player:
+    def __init__(self, human, items, directions):
+        self.human = human
+        self.items = items
+        self.directions = directions
+        
+        
 board = 0xaa55aa55aa55aa55
 our = [0x1, 0x4, 0x10, 0x40]
 their = [0x8000000000000000, 0x2000000000000000,
          0x0800000000000000, 0x0200000000000000]
 
+p1_human=True
+p2_human=False
+
+DIR_LT = 7
+DIR_RT = 9
+DIR_UP = True
+DIR_DN = False
 
 def display(val):
     s = format(val, 'b').zfill(64)[::-1]
@@ -14,7 +28,6 @@ def display(val):
         out = out + s[i - 8:i] + '\n'
     print(out)
 
-
 def show_board():
     o = sum(our)
     t = sum(their)
@@ -22,51 +35,64 @@ def show_board():
     ret = ''
     for i in range(0, 64):
         s = ' '
-        s = '.' if 1 & board >> i else ' '
+        s = '·' if 1 & board >> i else ' '
         if 1 & o >> i:
             s = 'o'
         if 1 & t >> i:
             s = '*'
         out = out + s
-    #        if i%8 == 0:
-    #            out = '\n' + out
     for i in range(64, 0, -8):
         ret = '{0}{1}\n'.format(ret, out[i - 8:i])
     print(ret)
 
+def ask():
+    direction = 0
+    n=0
+    n = input('Введите номер фигуры [{0}-{1}]:'.format(0, len(our)-1))
+    if n == 'q':
+        quit()
+    if n.isdigit():
+        print(n)
+        d = input("Введите направление движения (l или r):")
+        if d == 'l':
+            direction = DIR_LT
+        if d == 'r':
+            direction = DIR_RT
+        print(direction)
+    return (n, direction)
 
-def step_test(item, step):
-    if (item << step & (board ^ sum(our) ^ sum(their))) > 0:
-        return 1
-    else:
-        return -1
+def calculate(item, direction):
+    rr = rl = 0
+    cr = item<<DIR_RT if direction == DIR_UP else item>>DIR_RT
+    cl = item<<DIR_LT if direction == DIR_UP else item>>DIR_LT
+    
+    if step_allow(cr):
+        rr = 1 + calculate(cr, direction)
+    if step_allow(cl):
+        rl = 1 + calculate(cl, direction)
+    return (rr+rl)
 
+def step_allow(item):
+    return True if (item & (board ^ sum(our) ^ sum(their))) > 0 else False
 
-# def count_movies(table=_table):
-
-def step_our(item, direction):
-    incr = 9 if direction else 7
-    if step_test(our[item], incr) > 0:
-        our[item] = our[item] << incr
+def move_item(item, direction, incr):
+    c = our[item]<<incr if direction == DIR_UP else our[item]>>incr 
+    if step_allow(c):
+        our[item] = c
     else:
         print("Неверный ход...")
 
+if __name__ == "__main__": 
+    working = True
 
-def step_their():
-    none
-
-
-working = True
-while working:
+    while working:
 
     #    display(sum(our))
-    show_board()
-    nitem = input("Введите номер фигуры или q для выхода:")
-    if nitem == 'q':
-        break
-    if nitem.isdigit():
-        print(nitem)
-        direction = input("Введите направление движения (l или r):")
-        if direction == 'l' or direction == 'r':
-            print(direction)
-            step_our(int(nitem), True if direction == 'r' else False)
+        show_board()
+        if p1_human:
+            n,d = ask()
+            move_item(int(n), DIR_UP, d)
+
+
+
+        
