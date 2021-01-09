5b9aa5268876fc7db9dd2641a840ed7ef7bb9270
Овчинников Михаил Юрьевич <ovchinnikovmy@rushydro.ru>
2021-01-09 23:18:29 +0900

    Milestone...

--- stuckers.py
+++ stuckers.py
@@ -1,11 +1,38 @@
 # checkers-stuckers
 DIM = 8
-board = 0xaa55aa55aa55aa55
 
 DIR_LT = 7
 DIR_RT = 9
 DIR_UP = True
 DIR_DN = False
+
+
+class Table:
+    board = 0xaa55aa55aa55aa55
+    items = {}
+    
+    def show_board():
+        l = list(Table.items.values())
+        o = l[0]
+        t = l[1]
+        out = ''
+        ret = ''
+        for i in range(0, 64):
+            s = ' '
+            s = '·' if 1 & Table.board >> i else ' '
+            if 1 & o >> i:
+                s = 'o'
+            if 1 & t >> i:
+                s = '*'
+            out = out + s
+        for i in range(64, 0, -8):
+            ret = '{0}{1}\n'.format(ret, out[i - 8:i])
+        print(ret)
+
+    def allow(item):
+        b = sum(list(Table.items.values()))
+        return True if (item & (Table.board ^ b)) > 0 else False
+
 
 class Player:
 
@@ -18,58 +45,57 @@ class Player:
         self.items = items
         self.direction = direction
         self.homerow = 0x55 if self.direction == DIR_UP else 0xAA00000000000000
+        self.edgerow = 0xAA00000000000000 if self.direction == DIR_UP else 0x55
+        Table.items[self.name] = sum(self.items)
 
-    def move_item(self):
-        c = self.items[self.preferred_item]<<self.preferred_incr if self.direction == DIR_UP \
-        else self.items[self.preferred_item]>>self.preferred_incr 
-        if step_allow(c):
-            self.items[self.preferred_item] = c
-            return True
-        else:
-            print("{}: Неверный ход...".format(self.name))
-            return False
-      
     def ask(self):
-        n=0
-        n = input('Введите номер фигуры [{0}-{1}]:'.format(0, len(self.items)-1))
+        n=0 
+        n = input('{0}: Введите номер фигуры [{1}-{2}]:'.format(self.name, 0, len(self.items)-1))
         if n == 'q':
             return False
         if n.isdigit():
             self.preferred_item = int(n)
-            d = input("Введите направление движения (l или r):")
+            d = input("{0}: Введите направление движения (l или r):".format(self.name))
             if d == 'l':
                 self.preferred_incr = DIR_LT
             if d == 'r':
                 self.preferred_incr = DIR_RT
         return True
 
-p1=Player('1', True, [0x1, 0x4, 0x10, 0x40], DIR_UP)
-p2=Player('2', False, [0x8000000000000000, 0x2000000000000000,
-         0x0800000000000000, 0x0200000000000000], DIR_DN)
+    def move_item(self):
+        c = self.items[self.preferred_item]<<self.preferred_incr if self.direction == DIR_UP \
+        else self.items[self.preferred_item]>>self.preferred_incr 
+        if Table.allow(c):
+            if c & self.edgerow:
+                self.items.pop(self.preferred_item)
+                self.bifurcation()
+            else:
+                self.items[self.preferred_item] = c
+            Table.items[self.name] = sum(self.items)
+            return True
+        else:
+            print("{}: Неверный ход...".format(self.name))
+            return False
 
+    def bifurcation(self):
+        pass
+
+    def moving_items(self):
+        n = 0
+        if not self.items:
+            return 0
+        for i in self.items:
+            if Table.allow(i<<DIR_RT if self.direction == DIR_UP else i>>DIR_RT) or \
+                Table.allow(i<<DIR_LT if self.direction == DIR_UP else i>>DIR_LT):
+                n+=1
+        return n
+      
 def display(val):
     s = format(val, 'b').zfill(64)[::-1]
     out = ''
     for i in range(64, 0, -8):
         out = out + s[i - 8:i] + '\n'
     print(out)
-
-def show_board(sum_items1, sum_items2):
-    o = sum_items1
-    t = sum_items2
-    out = ''
-    ret = ''
-    for i in range(0, 64):
-        s = ' '
-        s = '·' if 1 & board >> i else ' '
-        if 1 & o >> i:
-            s = 'o'
-        if 1 & t >> i:
-            s = '*'
-        out = out + s
-    for i in range(64, 0, -8):
-        ret = '{0}{1}\n'.format(ret, out[i - 8:i])
-    print(ret)
 
 def calculate(item, direction):
     rr = rl = 0
@@ -82,19 +108,25 @@ def calculate(item, direction):
         rl = 1 + calculate(cl, direction)
     return (rr+rl)
 
-def step_allow(item):
-    return True if (item & (board ^ sum(p1.items) ^ sum(p2.items))) > 0 else False
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
+    
+    p1=Player('1', True, [0x1, 0x4, 0x10, 0x40], DIR_UP)
+#   p2=Player('2', False, [0x8000000000000000, 0x2000000000000000, 
+#             0x0800000000000000, 0x0200000000000000], DIR_DN)
+    p2=Player('2', False, [0x400000, 0x100000, 0x040000, 0x010000], DIR_DN)
     working = True
 
     while working:
-
-        show_board(sum(p1.items), sum(p2.items))
-        if p1.human:
-            p1.ask()
-            p1.move_item()
-
+        for p in [p1, p2]:
+            Table.show_board()
+            if not p.moving_items():
+                print("{}: You lose...".format(p.name))
+                quit()
+            if p.human:
+                while not p.ask():
+                    pass
+            p.move_item()
 
 
         
